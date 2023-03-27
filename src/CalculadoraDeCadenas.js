46dc16756487f128d5d39237c8284c0c7e2d47b7
younglee <young.lee@ucb.edu.bo>
2023-03-27 19:56:27 -0400

    aplicando refactorizacion

--- src/CalculadoraDeCadenas.js
+++ src/CalculadoraDeCadenas.js
@@ -1,15 +1,18 @@
 class CalculadoraDeCadenas{
     calcular(cadena){
-        let resultado = 0;
-        let numero="";
-        let delimitadorPersonalizado = "-";
+        this.delimitadorPersonalizado = "-";
         if(cadena[0] == "/" && cadena[1] == "/" && cadena[2] == "[" && cadena[4] == "]") {
-            delimitadorPersonalizado = cadena[3];
+            this.delimitadorPersonalizado = cadena[3];
             cadena = cadena.substring(5);
         }
+        return this.sumar(cadena);
+    }
+    sumar(cadena){
+        let resultado = 0;
+        let numero="";
         for (let i = 0; i < cadena.length; i++) {
             numero += cadena[i];
-            if(cadena[i] == "," || i == cadena.length-1 || cadena[i] == "-" || cadena[i] == delimitadorPersonalizado){
+            if(cadena[i] == "," || i == cadena.length-1 || cadena[i] == "-" || cadena[i] == this.delimitadorPersonalizado){
                 if(parseInt(numero)<=1000){
                     resultado +=parseInt(numero) ;
                 }
